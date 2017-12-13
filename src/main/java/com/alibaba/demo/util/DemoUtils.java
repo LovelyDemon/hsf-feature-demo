9518d4a1bd1c365d115dfbf7843dafba134aaff7
fu.guojun <fu.guojun@dtyunxi.com>
2017-12-13 18:10:02 +0800

    尝试修改提交

--- src/main/java/com/alibaba/demo/util/DemoUtils.java
+++ src/main/java/com/alibaba/demo/util/DemoUtils.java
@@ -9,5 +9,4 @@ public class DemoUtils {
 		// 这里设置Pandora地址，参数是sar包所在目录，如这里我的sar包地址是/Users/Jason/Work/AliSoft/PandoraSar/DevSar/taobao-hsf.sar，则只取前面的地址即可
 		return ServiceFactory.getInstanceWithPath(path);
 	}
-
 }
